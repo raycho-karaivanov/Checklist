8435bf6bdc1efc3e4c68a9449795c6a8e8c9b10a
Raycho Karaivanov <raycho@Raychos-Mac-mini.local>
2018-01-18 14:21:38 +0200

    FirstTime open the app - user experience added

--- Checklist/Checklist/Model/DataModel.swift
+++ Checklist/Checklist/Model/DataModel.swift
@@ -25,10 +25,28 @@ class DataModel {
     // MARK:- Set UserDefault default value
     
     func registerDefaults() {
-        let dictionary = [ "ChecklistIndex": -1]
+        
+        let dictionary: [String: Any] = [ "ChecklistIndex": -1, "FirstTime": true ]
         
         UserDefaults.standard.register(defaults: dictionary)
     }
+    
+    // MARK:- Handle First Time Use
+    
+    func handleFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
+    }
+    
     // MARK:- Load and Save Functions
     
     func documentsDirectory() -> URL {
@@ -67,6 +85,7 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleFirstTime()
     }
     
 }
